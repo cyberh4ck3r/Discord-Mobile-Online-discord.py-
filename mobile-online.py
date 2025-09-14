bfaa7275ce558b8b3a3db45daf6e0c8f3061adb6
[Agent] Ben <klacksmc@gmail.com>
2025-09-14 19:41:37 +0200

    Create mobile-online.py

---
+++ mobile-online.py
@@ -0,0 +1,28 @@
+from discord.gateway import DiscordWebSocket
+DiscordWebSocket.identify = identify
+
+async def identify(self):
+    payload = {
+        'op': self.IDENTIFY,
+        'd': {
+            'token': self.token,
+            'properties': {
+                'os': 'iOS',
+                'browser': 'Discord iOS',
+                'device': 'iPhone 16 Pro',
+            },
+            'compress': True,
+            'large_threshold': 250,
+        },
+    }
+
+    if self.shard_id is not None and self.shard_count is not None:
+        payload['d']['shard'] = [self.shard_id, self.shard_count]
+
+    state = self._connection
+
+    if state._intents is not None:
+        payload['d']['intents'] = state._intents.value
+
+    await self.call_hooks("before_identify", self.shard_id, initial=self._initial_identify)
+    await self.send_as_json(payload)
